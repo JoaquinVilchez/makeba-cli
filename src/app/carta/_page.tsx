c880f8960c7178f9a535a8b89a1ea45de080f78f
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2024-11-17 16:58:16 -0300

    UPDATE: Se redirige a link de entradas

---
+++ src/app/carta/_page.tsx
@@ -0,0 +1,12 @@
+
+// export default function Carta() {
+//   return (
+//     // <div style={{ height: "100vh", display: "flex", justifyContent:"center" }}>
+//     //     <iframe
+//     //     src="https://drive.google.com/file/d/1oN-y7WHVcgpSCASkrI3KKqdqRkayt0h4/preview"
+//     //     width="100%"
+//     //     height="100%"
+//     //   />
+//     // </div>
+//   );
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 'use client'
 
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-  const { push } = useRouter();
-
   useEffect(() => {
-     push('/carta');
-  });
+    window.location.href = "https://venti.com.ar/evento/makeba-15-de-junio"; // Redirige externamente
+  }, []); // Dependencia vacía para ejecutar solo al montar el componente
 
-  return (
-    <div />
-  );
+  return null; // Evita renderizar contenido visible
 }
